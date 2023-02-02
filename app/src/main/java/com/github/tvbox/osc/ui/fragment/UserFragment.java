0a27421b024ec3bfa1d9b985989b66d7ba82ca9b
pecially520 <62271580+pecially520@users.noreply.github.com>
2023-02-03 00:24:26 +0800

    Update UserFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
@@ -229,13 +229,13 @@ public class UserFragment extends BaseLazyFragment implements View.OnClickListen
         initHomeHotVod(homeHotVodAdapter);
 
         // takagen99: Initialize Icon Placement
-        boolean search_pos = Hawk.get(HawkConfig.HOME_SEARCH_POSITION, true);
+        boolean search_pos = Hawk.get(HawkConfig.HOME_SEARCH_POSITION, false);
         if (!search_pos) {
             tvSearch.setVisibility(View.VISIBLE);
         } else {
             tvSearch.setVisibility(View.GONE);
         }
-        boolean menu_pos = Hawk.get(HawkConfig.HOME_MENU_POSITION, true);
+        boolean menu_pos = Hawk.get(HawkConfig.HOME_MENU_POSITION, false);
         if (!menu_pos) {
             tvSetting.setVisibility(View.VISIBLE);
         } else {
